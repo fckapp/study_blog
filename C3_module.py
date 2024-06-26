32edff9be882a9de0dc7e940834bae886a041abd
Chris.Moon <160264916+fckapp@users.noreply.github.com>
2024-06-26 15:32:06 +0900

    Add files via upload

---
+++ C3_module.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+def precision_recall_curve_plot(y_test , pred_proba_c1):
+    # threshold ndarray와 이 threshold에 따른 정밀도, 재현율 ndarray 추출. 
+    precisions, recalls, thresholds = precision_recall_curve( y_test, pred_proba_c1)
+    
+    # X축을 threshold값으로, Y축은 정밀도, 재현율 값으로 각각 Plot 수행. 정밀도는 점선으로 표시
+    plt.figure(figsize=(8,6))
+    threshold_boundary = thresholds.shape[0]
+    plt.plot(thresholds, precisions[0:threshold_boundary], linestyle='--', label='precision')
+    plt.plot(thresholds, recalls[0:threshold_boundary],label='recall')
+    
+    # threshold 값 X 축의 Scale을 0.1 단위로 변경
+    start, end = plt.xlim()
+    plt.xticks(np.round(np.arange(start, end, 0.1),2))
+    
+    # x축, y축 label과 legend, 그리고 grid 설정
+    plt.xlabel('Threshold value'); plt.ylabel('Precision and Recall value')
+    plt.legend(); plt.grid()
+    plt.show()
+
+
+def get_clf_eval(y_test, pred=None, pred_proba=None):
+    confusion = confusion_matrix( y_test, pred)
+    accuracy = accuracy_score(y_test , pred)
+    precision = precision_score(y_test , pred)
+    recall = recall_score(y_test , pred)
+    f1 = f1_score(y_test,pred)
+    # ROC-AUC 추가 
+    roc_auc = roc_auc_score(y_test, pred_proba)
+    print('오차 행렬')
+    print(confusion)
+    # ROC-AUC print 추가
+    print('정확도: {0:.4f}, 정밀도: {1:.4f}, 재현율: {2:.4f},\
+          F1: {3:.4f}, AUC:{4:.4f}'.format(accuracy, precision, recall, f1, roc_auc))
+
+
+# 테스트를 수행할 모든 임곗값을 리스트 객체로 저장. 
+thresholds = [0.4, 0.45, 0.50, 0.55, 0.60]
+
+def get_eval_by_threshold(y_test , pred_proba_c1, thresholds):
+    # thresholds list객체내의 값을 차례로 iteration하면서 Evaluation 수행.
+    for custom_threshold in thresholds:
+        binarizer = Binarizer(threshold=custom_threshold).fit(pred_proba_c1) 
+        custom_predict = binarizer.transform(pred_proba_c1)
+        print('임곗값:',custom_threshold)
+        get_clf_eval(y_test , custom_predict)
